d32a67e7d045c239df1c7b9cb42ecb17e5026869
itschandrakanth <chandrakanth@socialprachar.com>
2022-05-04 16:44:12 +0530

    mobile nav buttons

---
+++ script.js
@@ -0,0 +1,4 @@
+/*for test no-js vs js just comment out this bit - codepen doesn't work without js enabled, so just have fun this way*/
+document.addEventListener("DOMContentLoaded",     function() {
+  //document.body.classList.add("js");
+});
